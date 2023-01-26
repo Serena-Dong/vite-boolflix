6b2fa65565515934a4e19b14f186b6182a814328
Serena-Dong <serenadong3159@icloud.com>
2023-01-26 17:10:16 +0100

    store.js

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import store from './data/data'
 
 export default {
+    data() {
+        return {
+            store
+        }
+    },
     components: { AppHeader, AppMain }
 }
 </script>
