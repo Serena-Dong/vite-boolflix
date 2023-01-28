2c7da72f40e874950e61330825c797db35eeeb6a
Serena-Dong <serenadong3159@icloud.com>
2023-01-28 12:42:08 +0100

    series and imgs

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ import { store } from './data/store'
 import { api } from './data/index'
 
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
+import card from './components/card.vue';
 
 export default {
     name: 'Boolflix',
-    components: { AppHeader, AppMain },
+    components: { AppHeader, card },
     data() {
         return {
             store,
@@ -31,13 +31,19 @@ export default {
         updateTerm(searchTerm) {
             this.term = searchTerm
         },
-        searchMovies() {
+        searchContent() {
             if (!this.term) {
-                store.movies = []
+                store.movies = [];
+                store.series = [];
             }
-            axios.get(`${api.baseUri}/search/movie`, this.parameters)
+
+            this.fetchApi('search/movie', 'movies');
+            this.fetchApi('search/tv', 'series');
+        },
+        fetchApi(endpoint, collection) {
+            axios.get(`${api.baseUri}/${endpoint}`, this.parameters)
                 .then(res => {
-                    store.movies = res.data.results
+                    store[collection] = res.data.results
                 })
         }
     }
@@ -45,9 +51,21 @@ export default {
 </script>
 
 <template>
-    <AppHeader @searching-bar="updateTerm" @form-submit="searchMovies"></AppHeader>
-    <AppMain>
-    </AppMain>
+    <AppHeader @searching-bar="updateTerm" @form-submit="searchContent"></AppHeader>
+
+
+
+    <main>
+
+        <!-- MOVIE SECTION -->
+        <h1>Movies</h1>
+        <card v-for="movie in store.movies" :key="movie.id" :content="movie"></card>
+
+        <!-- TV SERIES SECTION -->
+        <h1>TV Series</h1>
+        <card v-for="serie in store.series" :key="serie.id" :content="serie"></card>
+
+    </main>
 
 </template>
 
---
+++ src/components/card.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: 'card',
+    props: {
+        content: Object
+    },
+    computed: {
+        title() {
+            return this.content.title || this.content.name
+        },
+        originalTitle() {
+            return this.content.original_title || this.content.original_name
+        },
+        hasFlag() {
+            const flags = ['it', 'en']
+            return flags.includes(this.content.original_language)
+        },
+        imgSrc() {
+            const url = "../assets/img/" + this.content.original_language + ".png";
+            return url
+        }
+    }
+}
+</script>
+
+<template>
+    <ul>
+        <li>{{ title }}</li>
+        <li>{{ originalTitle }} </li>
+        <li>
+            <img v-if="hasFlag" :src="imgSrc">
+            <p v-else></p>
+        </li>
+        <li>{{ content.vote_average }}</li>
+
+    </ul>
+</template>
+
+<style>
+
+</style>
