dc0caf6a45557595094f25ad2594b2bea2f3ef05
Serena-Dong <serenadong3159@icloud.com>
2023-01-28 18:08:40 +0100

    font awesome removed from the main

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import './assets/css/style.css'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faHatWizard)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
 const app = createApp(App)
